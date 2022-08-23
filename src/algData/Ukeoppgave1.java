bc8be8f869050e4096f46a9b8970b1afcdaf72d5
tryma <s364596@oslomet.no>
2022-08-23 12:26:51 +0200

    First version with skeleton code

---
+++ src/algData/Ukeoppgave1.java
@@ -0,0 +1,18 @@
+package algData;
+
+public class Ukeoppgave1 {
+    public static void main(String[] args) {
+        int[] a = {8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19};
+
+        int min_indeks = min(a);
+
+        System.out.println(min_indeks);
+    }
+
+    static int min(int[] a) {
+        int min_value = a[0];
+        int min_indeks = 0;
+
+        return min_indeks;
+    }
+}
